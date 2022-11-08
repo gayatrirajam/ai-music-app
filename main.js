a4e95a715f46de4485b73523418a0f7cf48adacb
gayatrirajam <96321688+gayatrirajam@users.noreply.github.com>
2022-11-08 14:41:20 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var hedwigtheme = "";
 var starwarstheme = "";
+var leftwristX = "";
+var leftwristY = "";
+var rightwristX = "";
+var rightwristY = "";
 
 function preload(){
     hedwigtheme = loadSound("hedwig_s_theme.mp3");
@@ -10,6 +14,22 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log("Posenet is initialized!");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftwristX = results[0].pose.leftWrist.x;
+        leftwristY = results[0].pose.leftWrist.y;
+        rightwristX = results[0].pose.rightWrist.x;
+        rightwristY = results[0].pose.rightWrist.y;
+        console.log("LeftwristX: " + leftwristX + " LeftwristY: " + leftwristY + " RightwristX: " + rightwristX + " RightwristY: " + rightwristY);
+    }
 }
 function draw(){
     image(video, 0, 0, 700, 500);
