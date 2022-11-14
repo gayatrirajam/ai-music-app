aa8dbac097f28973463bc0160ce02be3a4650f72
gayatrirajam <96321688+gayatrirajam@users.noreply.github.com>
2022-11-13 19:57:47 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 var hedwigtheme = "";
+var hedwigtheme_check = "";
 var starwarstheme = "";
+var starwarstheme_check = "";
 var leftwristX = "";
 var leftwristY = "";
 var rightwristX = "";
 var rightwristY = "";
+var score_left_wrist = "";
 
 function preload(){
     hedwigtheme = loadSound("hedwig_s_theme.mp3");
@@ -24,6 +27,8 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        score_left_wrist = results[0].pose.keypoints[9].score;
+        console.log("Confidence of left wrist: " + score_left_wrist);
         leftwristX = results[0].pose.leftWrist.x;
         leftwristY = results[0].pose.leftWrist.y;
         rightwristX = results[0].pose.rightWrist.x;
@@ -33,4 +38,15 @@ function gotPoses(results){
 }
 function draw(){
     image(video, 0, 0, 700, 500);
+    starwarstheme_check = starwarstheme.isPlaying();
+    fill('red');
+    stroke('red');
+    if(score_left_wrist > 0.2){
+        circle(leftwristX, leftwristY, 25);
+        hedwigtheme.stop();
+        if(starwarstheme_check == false){
+            starwarstheme.play();
+            document.getElementById("songname").innerHTML = "Star Wars Theme";
+        }
+    }
 }
